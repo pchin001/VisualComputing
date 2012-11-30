46c36c4820e9ca90c40a0f3977e507d8ea1085a4
Patricia Chin <foxicle@hotmail.com>
2012-11-29 19:48:00 -0800

    Added new graphics project

---
+++ Graphics/Graphics/sceneModule.c
@@ -0,0 +1,87 @@
+/*
+ * OpenGL demonstration program for ICS Computer Graphics courses
+ * $Id: sceneModule.c,v 1.1 2002/04/03 00:00:43 mshafae Exp $
+ *
+ */
+
+#include "sceneModule.h"
+#include "inputModule.h"
+
+static float cubeColors[6][4] =
+{
+  {0.8, 0.8, 0.8, 1.0},
+  {0.8, 0.0, 0.0, 1.0},
+  {0.0, 0.8, 0.0, 1.0},
+  {0.0, 0.0, 0.8, 1.0},
+  {0.0, 0.8, 0.8, 1.0},
+  {0.8, 0.0, 0.8, 1.0},
+};
+
+static float cubeVertexes[6][4][4] =
+{
+  {
+    {-1.0, -1.0, -1.0, 1.0},
+    {-1.0, -1.0, 1.0, 1.0},
+    {-1.0, 1.0, 1.0, 1.0},
+    {-1.0, 1.0, -1.0, 1.0}},
+
+  {
+    {1.0, 1.0, 1.0, 1.0},
+    {1.0, -1.0, 1.0, 1.0},
+    {1.0, -1.0, -1.0, 1.0},
+    {1.0, 1.0, -1.0, 1.0}},
+
+  {
+    {-1.0, -1.0, -1.0, 1.0},
+    {1.0, -1.0, -1.0, 1.0},
+    {1.0, -1.0, 1.0, 1.0},
+    {-1.0, -1.0, 1.0, 1.0}},
+
+  {
+    {1.0, 1.0, 1.0, 1.0},
+    {1.0, 1.0, -1.0, 1.0},
+    {-1.0, 1.0, -1.0, 1.0},
+    {-1.0, 1.0, 1.0, 1.0}},
+
+  {
+    {-1.0, -1.0, -1.0, 1.0},
+    {-1.0, 1.0, -1.0, 1.0},
+    {1.0, 1.0, -1.0, 1.0},
+    {1.0, -1.0, -1.0, 1.0}},
+
+  {
+    {1.0, 1.0, 1.0, 1.0},
+    {-1.0, 1.0, 1.0, 1.0},
+    {-1.0, -1.0, 1.0, 1.0},
+    {1.0, -1.0, 1.0, 1.0}}
+};
+
+void drawCube( ){
+  int i;
+
+  for (i = 0; i < 6; ++i) {
+    glBegin(GL_POLYGON);
+    //glColor3fv(&cubeColors[i][0]);
+    glVertex4fv(&cubeVertexes[i][0][0]);
+    glVertex4fv(&cubeVertexes[i][1][0]);
+    glVertex4fv(&cubeVertexes[i][2][0]);
+    glVertex4fv(&cubeVertexes[i][3][0]);
+    glEnd();
+  }
+}
+
+void drawScene( ){
+	/* draw Spinner  */
+	glScalef(0.4, 0.4, 0.4);
+	glColor4f(1.0, 1.0, 0.0, 1.0);
+	drawCube();	
+
+	/* save Spinner state */
+	glPushMatrix();
+
+	/*draw Revolver */
+	glScalef(0.5, 0.5, 0.5);
+	glTranslatef(5.0, 0.0, 0.0);
+	glColor4f(1.0, 1.0, 1.0, 1.0);
+	drawCube();
+}
